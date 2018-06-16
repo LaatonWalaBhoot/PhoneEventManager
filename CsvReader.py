c68ecd6d8da7ffe86d2025dd24f0b666acb0f0b1
LaatonWalaBhoot <aishwarya.mailin@gmail.com>
2018-06-16 18:56:01 +0530

    Counted Unique Apps

--- CsvReader.py
+++ CsvReader.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import numpy as np
 
-#Counting Number of Phone Unlocks
+#Reading CSV
 df = pd.read_csv('Aishwarya - Problem Data - Sheet1.csv')
-df = df.event_name
-print(np.sum(df == "Phone_unlock"))
+
+#Counting Number of Phone Unlocks
+unlock_count = df.event_name
+print(np.sum(unlock_count == "Phone_unlock"))
+
+#Counting number of Apps Used
+app_count = df.event_name
+print(len(set(app_count))-4)
+
+#Calculating time spent away from phone
