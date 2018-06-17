df1c141c893d8f9c36854dba79b4e49a80028ba0
LaatonWalaBhoot <aishwarya.mailin@gmail.com>
2018-06-17 19:05:02 +0530

    Started working again

--- CsvReader.py
+++ CsvReader.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
-
+from dateutil.parser import parse
 #Reading CSV
 df = pd.read_csv('Aishwarya - Problem Data - Sheet1.csv')
 
 #Counting Number of Phone Unlocks
 unlock_count = df.event_name
-print(np.sum(unlock_count == "Phone_unlock"))
+print("Total number of times phone was unlocked: ",np.sum(unlock_count == "Phone_unlock"))
 
 #Counting number of Apps Used
 app_count = df.event_name
-print(len(set(app_count))-4)
-
-#Calculating time spent away from phone
+print("Total number of apps used: ", len(set(app_count))-4)
